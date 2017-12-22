7a2ea810fb3a13bef841a6ce073920e58ed09734
shivateja <shivateja.bluesilica@gmail.com>
2017-12-22 10:57:07 +0530

    edit added

--- src/pages/profile-page/profile-page.ts
+++ src/pages/profile-page/profile-page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 
 @Component({
     templateUrl:'profile-page.html'
@@ -9,7 +9,56 @@ export class ProfilePage{
 
     profile;
 
-    constructor(public navParams:NavParams){
+    constructor(public navParams:NavParams,public alertCtrl:AlertController){
         this.profile = this.navParams.get('profile');
+    }
+
+    edit(){
+        let prompt = this.alertCtrl.create({
+            title:'Edit Your Profile',
+            inputs:[
+                {
+                    name:'fathername',
+                    placeholder:'Father Name'
+                },
+                {
+                    name:'mothername',
+                    placeholder:'Mother Name'
+                },
+                {
+                    name:'mobile',
+                    placeholder:'Mobile Number'
+                },
+                {
+                    name:'email',
+                    placeholder:'EmailId'
+                },
+                {
+                    name:'bloodgroup',
+                    placeholder:'Blood Group'
+                },
+                {
+                    name:'address',
+                    placeholder:'Address'
+                }
+            ],
+            buttons:[
+                {
+                    text: 'Cancel'
+                },
+                {
+                    text: 'Save',
+                    handler: data=>{
+                        this.profile.fathername = data.fathername,
+                        this.profile.mothername = data.mothername,
+                        this.profile.mobileno = data.mobile,
+                        this.profile.email = data.email,
+                        this.profile.bloodgroup = data.bloodgroup,
+                        this.profile.address = data.address
+                    }
+                }
+            ]
+        });
+        prompt.present();
     }
 }
