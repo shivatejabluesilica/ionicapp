60707d5c7434b3381f27eb80254153cc70e5a3cc
shivateja <shivateja.bluesilica@gmail.com>
2017-12-26 14:50:09 +0530

    filechange added

--- src/pages/dummy/dummy.ts
+++ src/pages/dummy/dummy.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController,ActionSheetController,ToastController,
-    Platform,LoadingController,Loading} from 'ionic-angular';
+    Platform,LoadingController,Loading } from 'ionic-angular';
 
 import { File } from '@ionic-native/file';
 import { Transfer, TransferObject } from '@ionic-native/transfer';
@@ -17,122 +17,136 @@ declare var cordova: any;
 
 export class DummyPage{
 
-    lastImage: string = null;
-    loading: Loading;
+  lastImage: string = null;
+  loading: Loading;
+  img1;
 
-    constructor(public actionSheetCtrl:ActionSheetController,public navCtrl: NavController, private camera: Camera, private transfer: Transfer, 
-        private file: File, private filePath: FilePath,public toastCtrl: ToastController, public platform: Platform, 
-        public loadingCtrl: LoadingController, public appSettings:AppSettings){
-    }
+  constructor(public actionSheetCtrl:ActionSheetController,public navCtrl: NavController, private camera: Camera, private transfer: Transfer, 
+    private file: File, private filePath: FilePath,public toastCtrl: ToastController, public platform: Platform, 
+    public loadingCtrl: LoadingController, public appSettings:AppSettings){
+  }
 
-    public presentActionSheet() {
-        let actionSheet = this.actionSheetCtrl.create({
-          title: 'Select Image Source',
-          buttons: [
-            {
-              text: 'Load from Library',
-              handler: () => {
-                this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
-              }
-            },
-            {
-              text: 'Use Camera',
-              handler: () => {
-                this.takePicture(this.camera.PictureSourceType.CAMERA);
-              }
-            },
-            {
-              text: 'Cancel',
-              role: 'cancel'
-            }
-          ]
-        });
-        actionSheet.present();
-    }
-
-    public takePicture(sourceType) {
-      var options = {
-        quality: 100,
-        sourceType: sourceType,
-        saveToPhotoAlbum: false,
-        correctOrientation: true
-      };
-       
-      this.camera.getPicture(options).then((imagePath) => {
-        if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
-          this.filePath.resolveNativePath(imagePath)
-            .then(filePath => {
-              let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
-              let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
-              this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
-            });
-        } else {
-          var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
-          var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
-          this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+  public presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Image Source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
         }
-      }, (err) => {
-        this.presentToast('Error while selecting image.');
-      });
-    }
+      ]
+    });
+    actionSheet.present();
+  }
 
-    private createFileName() {
-      var d = new Date(),
-      n = d.getTime(),
-      newFileName =  n + ".jpg";
-      return newFileName;
-    }
-    
-    private copyFileToLocalDir(namePath, currentName, newFileName) {
-        this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
-          this.lastImage = newFileName;
-        }, error => {
-          this.presentToast('Error while storing file.');
-        });
-    }
-       
-    private presentToast(text) {
-        let toast = this.toastCtrl.create({
-          message: text,
-          duration: 3000,
-          position: 'top'
-        });
-        toast.present();
-    }
-    
-    public pathForImage(img) {
-        if (img === null) {
-          return '';
-        } else {
-          return cordova.file.dataDirectory + img;
-        }
-    }
+  public takePicture(sourceType) {
+    var options = {
+      quality: 100,
+      sourceType: sourceType,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+      
+    this.camera.getPicture(options).then((imagePath) => {
+      if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+        this.filePath.resolveNativePath(imagePath)
+          .then(filePath => {
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          });
+      } else {
+        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+      }
+    }, (err) => {
+      this.presentToast('Error while selecting image.');
+    });
+  }
 
-    public uploadImage() {
-        var url = this.appSettings.getApiUrl();
-        var targetPath = this.pathForImage(this.lastImage);
-        var filename = this.lastImage;
-       
-        var options = {
-          fileKey: "file",
-          fileName: filename,
-          chunkedMode: false,
-          mimeType: "multipart/form-data",
-          params : {'fileName': filename}
-        };
-       
-        const fileTransfer: TransferObject = this.transfer.create();
-       
-        this.loading = this.loadingCtrl.create({
-          content: 'Uploading...',
-        });
-        this.loading.present();
-        fileTransfer.upload(targetPath, url+'upload', options).then(data => {
-          this.loading.dismissAll()
-          this.presentToast('Image succesful uploaded.');
-        }, err => {
-          this.loading.dismissAll()
-          this.presentToast('Error while uploading file.');
-        });
+  private createFileName() {
+    var d = new Date(),
+    n = d.getTime(),
+    newFileName =  n + ".jpg";
+    return newFileName;
+  }
+    
+  private copyFileToLocalDir(namePath, currentName, newFileName) {
+    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+      this.lastImage = newFileName;
+    }, error => {
+      this.presentToast('Error while storing file.');
+    });
+  }
+      
+  private presentToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+    
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      return cordova.file.dataDirectory + img;
     }
+  }
+
+  public uploadImage() {
+    var url = this.appSettings.getApiUrl();
+    var targetPath = this.pathForImage(this.lastImage);
+    var filename = this.lastImage;
+    
+    var options = {
+      fileKey: "file",
+      fileName: filename,
+      chunkedMode: false,
+      mimeType: "multipart/form-data",
+      params : {'fileName': filename}
+    };
+    
+    const fileTransfer: TransferObject = this.transfer.create();
+    
+    this.loading = this.loadingCtrl.create({
+      content: 'Uploading...',
+    });
+    this.loading.present();
+    fileTransfer.upload(targetPath, url+'upload', options).then(data => {
+      this.loading.dismissAll()
+      this.presentToast('Image succesful uploaded.');
+    }, err => {
+      this.loading.dismissAll()
+      this.presentToast('Error while uploading file.');
+    });
+  }
+
+  fileChange(event){
+    if(event.target.files && event.target.files[0]){
+      let reader = new FileReader();
+
+      reader.onload = (event:any) => {
+        this.img1 = event.target.result;
+      }
+      reader.readAsDataURL(event.target.files[0]);
+    }
+      let fileList: FileList = event.target.files;  
+      console.log(fileList);
+  }
 }
